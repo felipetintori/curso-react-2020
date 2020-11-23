dd743ff8a338e5ce1a28f42df368bafe5c78c606
Felipe Andrade Gouvea Tintori <felipe.tintori@leomadeiras.com.br>
2020-11-23 11:01:09 -0300

    Usestate

--- react-funcional/src/App.js
+++ react-funcional/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+
+  const [numero, setNumero] = useState(0)
+  const [ segundoNumero, setSegundoNumero] = useState(10)
+  const [resultado, setResultado] = useState(15)
+
+  const somar = () => {
+    const numeroInt = parseInt(numero)
+    const segundoNumeroInt = parseInt(segundoNumero)
+
+    setResultado(numeroInt + segundoNumeroInt)
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      numero 1 <br/>
+      <input type="text" value={numero} onChange={e => setNumero(e.target.value)} /><br/>
+      numero 2 <br/>
+      <input type="text" value={segundoNumero} onChange={e => setSegundoNumero(e.target.value)} /><br/>
+      <button onClick={somar}>Somar</button> <br/>
+      resultado <br/>
+      <input type="text" value={resultado} /><br/>
+      
     </div>
   );
 }
